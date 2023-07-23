a6316f671e3934910b25a8d34893f1999dc95c80
Zach Garrison <zgedina@yahoo.com>
2023-07-23 14:18:54 -0500

    fixed

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,17 +1,22 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function MovieDetails() {
 
     const genres = useSelector(store => store.genres);
-    console.log(genres)
+    
     return (
         <div>
+            <Link to={`/`}>Back to Home</Link>
             <h1>Movie Details</h1>
             <img src={genres[0].movie_poster}/>
             <h1>{genres[0].movie_title}</h1>
-            <h2>{genres[0].genre_name}</h2>
+            <h2>
+                {genres.map((movie) => (
+                    <span key={movie.genre_name}>{movie.genre_name}<br></br> </span>
+                ))}
+            </h2>
             <h3>{genres[0].movie_description}</h3>
-
         </div>
     );
  }
